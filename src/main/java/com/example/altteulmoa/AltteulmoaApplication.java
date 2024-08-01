32f99039b45244d6d24a6359b1ddf3e36c3afbd8
postwo <codo7717@naver.com>
2024-08-01 12:30:01 +0900

    세팅

---
+++ src/main/java/com/example/altteulmoa/AltteulmoaApplication.java
@@ -0,0 +1,13 @@
+package com.example.altteulmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltteulmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltteulmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/altteulmoa/AltteulmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.altteulmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltteulmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
