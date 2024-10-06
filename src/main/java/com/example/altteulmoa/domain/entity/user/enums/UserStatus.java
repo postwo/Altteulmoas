305ea8dcba54955aa7cf36a7b4c323278c0a5a88
postwo <codo7717@naver.com>
2024-10-06 12:26:06 +0900

    시큐리티 권한 처리때문에 변경

--- src/main/java/com/example/altteulmoa/domain/entity/user/enums/UserStatus.java
+++ src/main/java/com/example/altteulmoa/domain/entity/user/enums/UserStatus.java
@@ -5,8 +5,9 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum UserStatus {
 
-    USER("일반 회원"),
-    ADMIN("관리자"),
+    ROLE_USER("일반 회원"),
+    ROLE_ADMIN("관리자"),
+    ROLE_MASTER("최고 관리자")
     ;
 
     private final String description;
